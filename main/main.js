0daa474c5f08be7de1f884762e8141877bfcbf0a
xjtu <313243401@qq.com>
2019-05-25 22:38:36 +0800

    version 1
    text fee

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+	var distance=inputs['distance'];
+	var parking=inputs['parkTime']*0.25;
+	var price=6;
+	if (distance<=2)price+=parking;
+		else if(distance<=8)price+=(distance-2)*0.8+parking;
+			else price+=0.8*6+1.2*(distance-8)+parking;
+    return Math.round(price);
 };
